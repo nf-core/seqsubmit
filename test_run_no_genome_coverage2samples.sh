93563a99f35841aa166a118662a75d4fee616c2a
Aleksandr Bykov <124188677+ribaribaworking@users.noreply.github.com>
2025-10-29 11:45:15 +0000

    #9 remove calculate_coverage module cz it's not needed anymore. Correct sample_sheets csvs

--- test_run_no_genome_coverage2samples.sh
+++ test_run_no_genome_coverage2samples.sh
@@ -2,7 +2,7 @@
 
 nextflow main.nf \
    -profile docker \
-   --input assets/samplesheet.csv \
+   --input assets/samplesheet_2_samples.csv \
    --outdir test_output \
    --ena_genome_study_accession PRJEB98843 \
-    -resume
+   -resume
