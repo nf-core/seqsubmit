30b97749c825f6951d9ccb8640dc2c978a044ee5
Aleksandr Bykov <124188677+ribaribaworking@users.noreply.github.com>
2025-10-29 12:01:30 +0000

    #9 clean sample sheets and test scripts

--- test_run_no_genome_coverage.sh
+++ test_run_no_genome_coverage.sh
@@ -2,7 +2,7 @@
 
 nextflow main.nf \
    -profile docker \
-   --input assets/samplesheet_test_missing_coverage.csv \
-   --outdir test_output \
+   --input assets/samplesheet_coverM_test.csv \
+   --outdir test_coverM_output \
    --ena_genome_study_accession PRJEB98843 \
    -resume
--- test_run_no_genome_coverage2samples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nextflow main.nf \
-   -profile docker \
-   --input assets/samplesheet_2_samples.csv \
-   --outdir test_output \
-   --ena_genome_study_accession PRJEB98843 \
-   -resume
