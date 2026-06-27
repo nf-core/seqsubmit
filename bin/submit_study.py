ab8f21272c854238af91545d4a26934d70c8f297
Ekaterina Sakharova <49755902+KateSakharova@users.noreply.github.com>
2026-06-27 22:03:18 +0100

    Update bin/submit_study.py
    Co-authored-by: Sofia Ochkalova <61541445+ochkalova@users.noreply.github.com>

--- bin/submit_study.py
+++ bin/submit_study.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-## Originally written by Tim Rozday and released under the MIT license.
+## Originally written by Tim Rozday, modified by Sofiia Ochkalova and released under the MIT license.
 ## See git repository (https://github.com/nf-core/seqsubmit) for full license text.
 
 
